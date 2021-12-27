e856878a12042fb255c609973b2dd222e3c027c7
WilliamTouboul <will.touboul@gmail.com>
2021-12-27 20:50:45 +0100

    first push

---
+++ assets/scripts/scrollTrigger.js
@@ -0,0 +1,52 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+
+gsap.to(".bgHero", {
+    scrollTrigger: {
+        trigger: ".bgHero",
+        start: "top center",
+        scrub: true,
+    },
+    y: -350
+});
+
+
+
+gsap.to(".backtitleSection2", {
+    scrollTrigger: {
+        trigger: ".backtitleSection2",
+        start: "top 75%",
+        scrub: true,
+    },
+    y: -150
+});
+
+
+gsap.to('#imageProject1', {
+    scrollTrigger: {
+        trigger: "#imageProject1",
+        start: "top 80%",
+        scrub: true
+    },
+    y: 50
+});
+
+gsap.to('#imageProject2', {
+    scrollTrigger: {
+        trigger: "#imageProject3",
+        start: "top 80%",
+        scrub: true
+    },
+    y: 50
+});
+
+gsap.to('#imageProject3', {
+    scrollTrigger: {
+        trigger: "#imageProject3",
+        start: "top 80%",
+        scrub: true
+    },
+    y: 50
+});
